bd2697d149c475214ca17d0bf7a06964acc6fd09
uiuiuiui15 <dr.venitus@gmail.com>
2024-11-30 01:19:33 +0500

    a

---
+++ Tyuiu.VorobevAD.Sprint4.Task6.V3.Lib/DataService.cs
@@ -0,0 +1,12 @@
+﻿using tyuiu.cources.programming.interfaces.Sprint4;
+
+namespace Tyuiu.VorobevAD.Sprint4.Task6.V3.Lib;
+
+public class DataService : tyuiu.cources.programming.interfaces.Sprint4.ISprint4Task6V3
+{
+	public int Calculate(string[] array)
+	{
+		return array.Count(e => e.Length < 6);
+		throw new NotImplementedException();
+	}
+}
