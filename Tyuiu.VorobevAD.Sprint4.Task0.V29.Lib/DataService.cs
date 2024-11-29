b729d9289f0891fde75e05233a2e7b7e8391841d
uiuiuiui15 <dr.venitus@gmail.com>
2024-11-29 22:26:04 +0500

    Project init

---
+++ Tyuiu.VorobevAD.Sprint4.Task0.V29.Lib/DataService.cs
@@ -0,0 +1,17 @@
+﻿using tyuiu.cources.programming.interfaces.Sprint4;
+
+namespace Tyuiu.VorobevAD.Sprint4.Task0.V29.Lib;
+
+public class DataService : tyuiu.cources.programming.interfaces.Sprint4.ISprint4Task0V29
+{
+	public int GetSumOddArrEl(int[] array)
+	{
+		int acc = 0
+		foreach (int i in array)
+		{
+			if (i % 2 != 0) acc += i;
+		}
+		return acc;
+		throw new NotImplementedException();
+	}
+}
