4ad06bb00c977334d30f6f40d29ae1a2c51b6e5b
uiuiuiui15 <dr.venitus@gmail.com>
2024-11-29 23:28:03 +0500

    b

--- Tyuiu.VorobevAD.Sprint4.Task2.V30.Lib/DataService.cs
+++ Tyuiu.VorobevAD.Sprint4.Task2.V30.Lib/DataService.cs
@@ -1,6 +1,19 @@
-﻿namespace Tyuiu.VorobevAD.Sprint4.Task2.V30.Lib;
+﻿using tyuiu.cources.programming.interfaces.Sprint4;
 
-public class Class1
+namespace Tyuiu.VorobevAD.Sprint4.Task2.V30.Lib;
+
+public class DataService : tyuiu.cources.programming.interfaces.Sprint4.ISprint4Task2V30
 {
+	public int Calculate(int[] array)
+	{
+		int acc = 0;
 
+		for (int i = 0; i < array.Length; ++i)
+		{
+			if (i % 2 != 0) acc *= array[i];
+		}
+
+		return acc;
+		throw new NotImplementedException();
+	}
 }
