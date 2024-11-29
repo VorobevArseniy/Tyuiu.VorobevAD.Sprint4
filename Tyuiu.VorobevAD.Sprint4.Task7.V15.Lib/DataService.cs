3642e81be6bfbcd257afd0b147d6a2deb2f33a4f
uiuiuiui15 <dr.venitus@gmail.com>
2024-11-30 01:32:57 +0500

    a

---
+++ Tyuiu.VorobevAD.Sprint4.Task7.V15.Lib/DataService.cs
@@ -0,0 +1,26 @@
+﻿using tyuiu.cources.programming.interfaces.Sprint4;
+
+namespace Tyuiu.VorobevAD.Sprint4.Task7.V15.Lib;
+
+public class DataService : tyuiu.cources.programming.interfaces.Sprint4.ISprint4Task7V15
+{
+	public int Calculate(int n, int m, string value)
+	{
+		int[,] matrix = new int[n, m];
+		int count = 0;
+		for (int i = 0; i < n; ++i)
+		{
+			for (int j = 0; j < m; ++j)
+			{
+				matrix[i, j] = int.Parse(value[i * n + j].ToString());
+
+				if (matrix[i, j] % 2 != 0)
+				{
+					count++;
+				}
+			}
+		}
+		return count;
+		throw new NotImplementedException();
+	}
+}
